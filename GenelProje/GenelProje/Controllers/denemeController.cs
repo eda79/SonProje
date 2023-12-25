b043df30728fd46357060df4669eafa43de62d60
eda79 <97768384+eda79@users.noreply.github.com>
2023-12-25 17:24:34 +0300

    SonProje
    SonProje

---
+++ GenelProje/GenelProje/Controllers/denemeController.cs
@@ -0,0 +1,64 @@
+﻿using GenelProje.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenelProje.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class denemeController : ControllerBase
+    {
+
+        private readonly NorthwndContext _northwndContext;
+        public denemeController(NorthwndContext northwndContext)
+        {
+            _northwndContext = northwndContext;
+        }
+
+        [HttpGet("deneme")]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+
+
+        //[HttpPost("start")]
+        //public IActionResult StartPeriodicTask()
+        //{
+
+        //    //RecurringJob.AddOrUpdate<IServiceManagement>(x => x.PeriodicTask(), Cron.MinuteInterval(1));
+        //    //RecurringJob.AddOrUpdate("periodic-task", () => PeriodicTask(), "*/5 * * * *");
+        //    RecurringJob.AddOrUpdate("periodic-task", () => PeriodicTask(), Cron.MinuteInterval(1));
+
+
+        //    return Ok();
+        //}
+
+
+        //[HttpPost("stop")]
+        //public IActionResult StopPeriodicTask()
+        //{
+        //    // Hangfire'da tanımlanan belirli bir görevi durdur
+        //    RecurringJob.RemoveIfExists("periodic-task");// belirtilen isimde bir görev varsa onu durdurur.
+
+        //    return Ok();
+        //}
+
+        //public void PeriodicTask()
+        //{
+
+        //    //Hangfire tarafından belirli aralıklarla çalıştırılacak olan işlemler
+
+        //    // güncelleme işlemi
+        //    var recordsToUpdate = _northwndContext.Products.ToList();
+
+        //    foreach (var record in recordsToUpdate)
+        //    {
+        //        record.UnitPrice++;
+        //    }
+
+        //    _northwndContext.SaveChanges();
+        //}
+    }
+}
